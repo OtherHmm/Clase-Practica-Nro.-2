5a27212b75e193f354c8f38be0deba7ba45b0fb4
OtherHmm <syrvm9@gmail.com>
2024-09-03 18:13:59 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_Practica_Nro._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Salir = false;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Buenas, cuales ejercicos quiere ver :P :");
+                Console.WriteLine("");
+                Console.WriteLine("1. Primeros ejercicios");
+                Console.WriteLine("2. Segundos ejercicios");
+                Console.WriteLine("");
+                Console.WriteLine("0. Salir");
+                byte opcMain = Convert.ToByte(Console.ReadLine());
+
+                switch (opcMain)
+                {
+                    case 0:
+                        Salir = true;
+                        break;
+                    case 1:
+                        Console.Clear();
+
+                        Console.WriteLine("Que inciso del ejercico");
+                        Console.WriteLine("");
+                        Console.WriteLine("a. Calcular factorial");
+                        Console.WriteLine("b. Crear tabla de un numero");
+                        Console.WriteLine("c. Imprimir serie de Fibonacci");
+                        Console.WriteLine("");
+                        Console.WriteLine("s. Salir");
+                        string opc = Console.ReadLine();
+
+                        switch (opc)
+                        {
+                            case "s":
+                                Console.Clear();
+                                Salir = true;
+                                break;
+                            case "a":
+                                Factorial();
+                                break;
+                            case "b":
+                                Tabla();
+                                break;
+                            case "c":
+                                Fibonacci();
+                                break;
+                            default:
+                                MensajeError("Opcion Invalida, Eliga otra");
+                                break;
+                        }
+                        break;
+                    case 2:
+                        Console.Clear();
+
+                        Console.WriteLine("Que inciso del ejercico");
+                        Console.WriteLine("");
+                        Console.WriteLine("a. Calcular area de figuras");
+                        Console.WriteLine("b. Conversion de unidades");
+                        Console.WriteLine("c. Ordenar numeros o palabras");
+                        Console.WriteLine("d. Cambiar formato de fechas");
+                        Console.WriteLine("");
+                        Console.WriteLine("s. Salir");
+                        string opcSec = Console.ReadLine();
+
+                        switch (opcSec)
+                        {
+                            case "s":
+                                Salir = true;
+                                break;
+                            case "a":
+                                CalcularArea();
+                                break;
+                            case "b":
+                                ConversionMedidas();
+                                break;
+                            case "c":
+                                Reordenar();
+                                break;
+                            case "d":
+                                CambiarFormato();
+                                break;
+                            default:
+                                MensajeError("Opcion Invalida, Eliga otra");
+                                break;
+                        }
+                        break;
+                    default:
+                        MensajeError("Opcion Invalida, Eliga otra");
+                        break;
+                }
+            } while (!Salir);
+        }
+        // primeros ejercicios
+
+        static void Factorial()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Ingrese el numero que quieres calcular su factorial");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            int numFact = FactorialFormula(numero);
+
+            Console.WriteLine($"EL factorial de {numero} es : {numFact}");
+            Console.ReadKey();
+
+        }
+        static int FactorialFormula(int numero)
+        {
+            if (numero == 1)
+            {
+                return numero;
+            }
+            else
+            {
+                return numero * FactorialFormula(numero - 1);
+            }
+        }
+        static void Tabla()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Ingrese el numero que quieres calcular su tabla de multiplicar");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            int n = 1;
+
+            Console.WriteLine($"Tabla del {numero}");
+
+            TablaImprimir(numero, n);
+            Console.ReadKey();
+
+        }
+        static void TablaImprimir(int numero, int n)
+        {
+            if (n > 10)
+                return;
+            Console.WriteLine($"{numero} x {n} = {numero * n}");
+
+            TablaImprimir(numero, n + 1);
+        }
+        static void Fibonacci()
+        {
+            Console.Clear();
+
+            Console.WriteLine("¿Cuántos términos de la serie de Fibonacci deseas mostrar? ");
+            int Cant = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Serie de Fibonacci:");
+            for (int i = 0; i < Cant; i++)
+            {
+                Console.Write(FibonacciFormula(i) + " ");
+            }
+            Console.ReadKey();
+
+        }
+        static int FibonacciFormula(int n)
+        {
+            if (n <= 1)
+                return n;
+            else // formula de Fibonacci F(n) = F(n−1) + F(n−2)
+                return FibonacciFormula(n - 1) + FibonacciFormula(n - 2);
+        }
+       
+        // segundos ejercicios
+
+        static void CalcularArea()
+        {
+            
+        }
+        static void ConversionMedidas()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Digite la cantidad de unidades");
+            int unidades = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite la medida base");
+            string medidaBase = Console.ReadLine();
+            Console.WriteLine("Digite la medida nueva");
+            string medidaNueva = Console.ReadLine();
+
+            double resultado = Convert.ToDouble(ConversionProceso(unidades, medidaBase, medidaNueva));
+
+            Console.WriteLine($"{unidades} {medidaBase} en {medidaNueva} son {resultado} {medidaNueva}");
+            Console.ReadKey(); 
+        }
+        static double ConversionProceso(int cant, string medidaBase, string medidaNueva)
+        {
+            if (medidaBase == "cm" && medidaNueva    == "m")
+                return cant / 100;
+            if (medidaBase == "m" && medidaNueva == "km")
+                return cant / 1000;
+            if (medidaBase   == "km" && medidaNueva == "m")
+                return cant * 1000;
+            if (medidaBase == "m" && medidaNueva == "cm")
+                return cant * 100;
+
+            Console.WriteLine("Medida incorrecta o desconocida, pruebe con: cm, m o km");
+            return cant;
+
+        }
+        static void Reordenar()
+        {
+        
+        }
+        static void CambiarFormato()
+        {
+        
+        }
+        
+        // general
+
+        static void MensajeError(string msg)
+        {
+            Console.Clear();
+            Console.WriteLine(msg);
+            Console.ReadKey();
+        }
+    }
+}
